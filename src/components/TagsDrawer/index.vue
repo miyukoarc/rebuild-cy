3f25daa960698bbb5eea8dde0fb02270f00dcfd4
Oscar <oscarcgm@hotmail.com>
2020-07-09 18:47:36 +0800

    7/9 fix tagDrawer

--- src/components/TagsDrawer/index.vue
+++ src/components/TagsDrawer/index.vue
@@ -1,25 +1,47 @@
 <template>
   <div>
     <div class="drawer-container" v-if="alterGroups != null">
-      <div class="drawer-item" v-for="(group,key,index) in alterGroups" :key="index">
-        <el-row type="flex" class="row-bg" justify="center">
-          <el-col :span="8">
-            <div class="font-exs color-info group-name">{{group.groupName}}：</div>
-          </el-col>
-          <el-col :span="16">
-            <div class="tags-container">
-              <el-tag
-                class="tag-unit text-ellipsis"
-                type="info"
-                size="mini"
-                v-for="tag in alterTags(group.tags)"
-                :key="tag.tagId"
-              >{{tag.tagName}}</el-tag>
-            </div>
-          </el-col>
-        </el-row>
+      <div v-if="propsType">
+        <div class="drawer-item" v-for="(group,key,index) in alterGroups" :key="index">
+          <el-row type="flex" class="row-bg" justify="center">
+            <el-col :span="8">
+              <div class="font-exs color-info group-name">{{group.groupName}}：</div>
+            </el-col>
+            <el-col :span="16">
+              <div class="tags-container">
+                <el-tag
+                  class="tag-unit text-ellipsis"
+                  type="info"
+                  size="mini"
+                  v-for="tag in alterTags(group.tags)"
+                  :key="tag.tagId"
+                >{{tag.tagName}}</el-tag>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
       </div>
-      <div class="text-align-center" v-if="tags.length>2">
+      <div v-else>
+        <div class="drawer-item" v-for="(group,key,index) in alterGroups" :key="index">
+          <el-row type="flex" class="row-bg" justify="center">
+            <el-col :span="8">
+              <div class="font-exs color-info group-name">{{key}}：</div>
+            </el-col>
+            <el-col :span="16">
+              <div class="tags-container">
+                <el-tag
+                  class="tag-unit text-ellipsis"
+                  type="info"
+                  size="mini"
+                  v-for="tag in alterTags(group.tags)"
+                  :key="tag.tagId"
+                >{{tag.tagName}}</el-tag>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+      </div>
+      <div class="text-align-center" v-if="Object.values(tags).length>2">
         <el-button type="text" size="mini" @click="curly=!curly">
           展开
           <i :class="[curly?'el-icon-caret-bottom':'el-icon-caret-top']"></i>
@@ -32,27 +54,20 @@
 
 <script>
 export default {
-  props: {
-    tags: {
-      type: Object,
-      default: () => {
-        return [];
-      }
-    }
-  },
-  name: "tags-drawer",
+  props: ['tags'],
+  name: 'tags-drawer',
   data() {
     return {
       curly: false
-    };
+    }
   },
   watch: {
     tags: {
       handler(newVal, oldVal) {
-         if (newVal.length > 2) {
-          this.curly = true;
+        if (newVal.length > 2) {
+          this.curly = true
         } else {
-          this.curly = false;
+          this.curly = false
         }
       },
       immediate: true
@@ -60,26 +75,29 @@ export default {
   },
   computed: {
     total() {
-      return this.tags.length;
+      return this.tags.length
     },
     alterGroups() {
       if (this.curly) {
-        return this.tags.slice(0, 2);
+        return this.tags.slice(0, 2)
       } else {
-        return this.tags;
+        return this.tags
       }
+    },
+    propsType() {
+      return Array.isArray(this.tags)
     }
   },
   methods: {
     alterTags(arr) {
       if (this.curly) {
-        return arr.slice(0, 1);
+        return arr.slice(0, 1)
       } else {
-        return arr;
+        return arr
       }
     }
   }
-};
+}
 </script>
 
 <style>
