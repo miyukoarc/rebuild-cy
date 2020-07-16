db31639b11e195196485965ad39c23667fc76f6e
Oscar <oscarcgm@hotmail.com>
2020-07-16 20:15:01 +0800

    7/16 merge enum

--- src/store/modules/enum.js
+++ src/store/modules/enum.js
@@ -8,87 +8,69 @@
  */
 const state = {
 
-    auditState: {
-        TO_BE_REVIEWED: '待审核',
-        APPROVED: '审核通过',
-        AUDIT_FAILED: '审核不通过'
-    },
-    mediaType: {
-        IMAGE: '图片',
-        VOICE: '声音',
-        FILE: '文件',
-        TEXT: '文本',
-        ARTICLE: '文章'
-    },
-    roles: {
-        'super': '超级管理员',
-        'employee': '员工'
-    },
-    roleDetail: {
-        customerStatistics: '客户统计',
-        externalUserTrends: '客户动态',
-        quitUserRelationExUserList: '离职成员列表',
-        redistributionExUser: '再分配联系人',
-        audit: '审核',
-        batchSendTask: '群发任务',
-        department: '部门',
-        externalUser: '客户',
-        groupChat: '群聊',
-        log: '日志',
-        media: '素材',
-        mediaGroup: '素材组',
-        message: '会话',
-        permission: '权限',
-        potentialCustomer: '潜在客户',
-        role: '角色',
-        tag: '标签',
-        user: '用户',
-        sensitive: '敏感操作',
-        menu: '菜单',
-        contactWay: '渠道'
-    },
-    orgType: {
-        HEAD: '总公司',
-        BRANCH: '分公司',
-        BUSINESS: '营业部',
-        DEPT: '普通部门'
-    },
-    quitUserRelationExUserListUserStatus: {
-        ALLOCATE: '离职待分配',
-        ALLOCATED: '离职已分配'
-}，
-    mediaType: {
-        IMAGE:'图片',
-        FILE: '文件',
-        VIDEO: '视频',
-        ARTICLE: '文章',
-        TEXT:'文本'
-    }，
-    batchAddTask: {
-        NOTSTARTED: "未开始",
-        INTERRUPT: "中断",
-        PROCESSING: "进行中",
-        CANCELED: "已取消",
-        FINISHED: "已完成",
-    }
-<<<<<<< .mine
-    },
-    mediaType: {
-        IMAGE:'图片',
-        FILE: '文件',
-        VIDEO: '视频',
-        ARTICLE: '文章',
-        TEXT:'文本'
-=======
-    },
-    batchAddTask: {
-        NOTSTARTED: "未开始",
-        INTERRUPT: "中断",
-        PROCESSING: "进行中",
-        CANCELED: "已取消",
-        FINISHED: "已完成",
->>>>>>> .theirs
-    }
+  auditState: {
+    TO_BE_REVIEWED: '待审核',
+    APPROVED: '审核通过',
+    AUDIT_FAILED: '审核不通过'
+  },
+  mediaType: {
+    IMAGE: '图片',
+    VOICE: '声音',
+    FILE: '文件',
+    TEXT: '文本',
+    ARTICLE: '文章'
+  },
+  roles: {
+    'super': '超级管理员',
+    'employee': '员工'
+  },
+  roleDetail: {
+    customerStatistics: '客户统计',
+    externalUserTrends: '客户动态',
+    quitUserRelationExUserList: '离职成员列表',
+    redistributionExUser: '再分配联系人',
+    audit: '审核',
+    batchSendTask: '群发任务',
+    department: '部门',
+    externalUser: '客户',
+    groupChat: '群聊',
+    log: '日志',
+    media: '素材',
+    mediaGroup: '素材组',
+    message: '会话',
+    permission: '权限',
+    potentialCustomer: '潜在客户',
+    role: '角色',
+    tag: '标签',
+    user: '用户',
+    sensitive: '敏感操作',
+    menu: '菜单',
+    contactWay: '渠道'
+  },
+  orgType: {
+    HEAD: '总公司',
+    BRANCH: '分公司',
+    BUSINESS: '营业部',
+    DEPT: '普通部门'
+  },
+  quitUserRelationExUserListUserStatus: {
+    ALLOCATE: '离职待分配',
+    ALLOCATED: '离职已分配'
+  },
+  mediaType: {
+    IMAGE: '图片',
+    FILE: '文件',
+    VIDEO: '视频',
+    ARTICLE: '文章',
+    TEXT: '文本'
+  },
+  batchAddTask: {
+    NOTSTARTED: "未开始",
+    INTERRUPT: "中断",
+    PROCESSING: "进行中",
+    CANCELED: "已取消",
+    FINISHED: "已完成",
+  }
 }
 
 const mutations = {
@@ -100,8 +82,8 @@ const actions = {
 }
 
 export default {
-    namespaced: true,
-    state,
-    mutations,
-    actions
+  namespaced: true,
+  state,
+  mutations,
+  actions
 }
