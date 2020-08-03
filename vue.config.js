a09a6809699817ff992f5e8def2bfbd5b0ef443c
xahello96 <1999wxa1999@163.com>
2020-08-03 20:57:15 +0800

    123

--- vue.config.js
+++ vue.config.js
@@ -57,8 +57,8 @@ const customizeDevServer = process.env.VUE_APP_WORK === 'offline' ? {
         [process.env.VUE_APP_BASE_API]: {
             // target: 'http://47.111.161.128:9000/',
             // target: 'http://10.10.10.4:9000', // 光
-            target: 'http://10.10.10.41', // 杜亮
-            // target: 'http://10.10.10.198', // 骋昊
+            // target: 'http://10.10.10.41', // 杜亮
+            target: 'http://10.10.10.198', // 骋昊
             // ws: true,
             changeOrigin: true,
             pathRewrite: {
@@ -85,9 +85,9 @@ const customizeDevServer = process.env.VUE_APP_WORK === 'offline' ? {
                 Host: 'sidebar.cyscrm.com'
             },
             // target: 'http://47.111.161.128:9000/',
-            // target: "http://10.10.10.198", //陈浩
+            target: "http://10.10.10.198", //陈浩
             // target: "http://sidebar.cyscrm.com:9000"//线上
-            target: "http://10.10.10.41", //杜亮
+            // target: "http://10.10.10.41", //杜亮
             //   target: 'http://10.10.10.4:9000', //光
             // target: "http://47.111.161.128:80",
             secure: false,
@@ -101,9 +101,9 @@ const customizeDevServer = process.env.VUE_APP_WORK === 'offline' ? {
                 Host: 'sidebar.cyscrm.com'
             },
             // target: 'http://47.111.161.128:9000/',
-            // target: "http://10.10.10.198", //陈浩
+            target: "http://10.10.10.198", //陈浩
             // target: "http://sidebar.cyscrm.com:9000"//线上
-            target: "http://10.10.10.41", //杜亮
+            // target: "http://10.10.10.41", //杜亮
             // target: 'http://10.10.10.4:9000', //光
             // target: "http://47.111.161.128:80",
             secure: false,
