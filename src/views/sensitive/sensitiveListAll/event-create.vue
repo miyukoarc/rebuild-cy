50409981aff6528605f53c164cf49dd9085ac98b
zeness <https://zeness@gitee.com>
2020-08-14 16:18:48 +0800

    修改通知人

--- src/views/sensitive/sensitiveListAll/event-create.vue
+++ src/views/sensitive/sensitiveListAll/event-create.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-form :model="form" ref="form" :rules="rules" label-width="100px" label-position="left">
+  <el-form :model="form" ref="form" :rules="rules" label-width="80px" label-position="left">
     <el-form-item label="敏感词">
       <el-tag
         :key="tag"
@@ -24,33 +24,38 @@
       </el-button>
     </el-form-item>
     <el-form-item label="被通知人">
-      <div>
-        <el-radio v-model="form.informType" label="USER">员工</el-radio>
+      <div class="display-flex align-items-center">
+        <!-- <el-radio v-model="form.informType" label="USER">员工</el-radio> -->
         <el-radio v-model="form.informType" label="ROLE">角色</el-radio>
+        <div>
+          <strong class="font-es">推送上级：</strong>
+          <el-switch v-model="form.noticeType"></el-switch>
+          <span class="font-exs ml-5">开启开关后，上级部门将受到触发敏感词通知</span>
+        </div>
       </div>
     </el-form-item>
 
-    <el-form-item label></el-form-item>
-    <div class="mb-20">
-      <keep-alive>
-        <div class="role-container mb-20" v-if="form.informType=='ROLE'">
-          <el-select v-model="toRoles" multiple>
-            <el-option
-              v-for="item in listSelect"
-              :key="item.uuid"
-              :label="item.name"
-              :value="item.uuid"
-            ></el-option>
-          </el-select>
-        </div>
-        <div v-else>
-          <multi-tree-select v-model="userSelects" :section="'user'" :multiple="true"></multi-tree-select>
+    <el-form-item label=" ">
+      <div>
+        <keep-alive>
+          <div class="role-container">
+            <el-select v-model="toRoles" multiple>
+              <el-option
+                v-for="item in listSelect"
+                :key="item.uuid"
+                :label="item.name"
+                :value="item.uuid"
+              ></el-option>
+            </el-select>
+          </div>
+          <!-- <div v-else> -->
+          <!-- <multi-tree-select v-model="userSelects" :section="'user'" :multiple="true"></multi-tree-select> -->
           <!-- <complex-select v-model="userSelects" :section="'user'" :options="departmentList"></complex-select> -->
-        </div>
-      </keep-alive>
-      <span class="font-exs color-info">当员工触发敏感词后，除通知以上设置被通知人，触发的员工本人也将收到通知消息。</span>
-    </div>
-
+          <!-- </div> -->
+        </keep-alive>
+        <span class="font-exs color-info">当员工触发敏感词后，除通知以上设置被通知人，触发的员工本人也将收到通知消息。</span>
+      </div>
+    </el-form-item>
     <el-form-item label="符合标签">
       <div>
         <el-radio v-model="form.tagType" label="INSET">包含其一</el-radio>
@@ -67,14 +72,14 @@
 </template>
 
 <script>
-import { mapState } from 'vuex'
-import ComplexSelect from '@/components/ComplexSelect'
-import AsyncUserTag from '@/components/AsyncUserTag'
-import TagMultiSelect from '@/components/TagMultiSelect'
-import MultiTreeSelect from '@/components/MultiTreeSelect'
+import { mapState } from "vuex";
+import ComplexSelect from "@/components/ComplexSelect";
+import AsyncUserTag from "@/components/AsyncUserTag";
+import TagMultiSelect from "@/components/TagMultiSelect";
+import MultiTreeSelect from "@/components/MultiTreeSelect";
 
 export default {
-  inject: ['reload'],
+  inject: ["reload"],
   components: {
     AsyncUserTag,
     ComplexSelect,
@@ -86,47 +91,48 @@ export default {
       userSelects: [],
       tagSelects: [],
       inputVisible: false,
-      inputValue: '',
+      inputValue: "",
       form: {
-        tagType: 'INSET', //INSET UNIONSET
-        informType: 'USER', //USER,ROLE
-        type: 'MSG',
+        tagType: "INSET", //INSET UNIONSET
+        informType: "ROLE", //USER,ROLE
+        type: "MSG",
         words: [],
+        noticeType: true,
       },
       toUserTags: [],
       toRoles: [],
       rules: {
         word: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { required: true, message: "请输入活动名称", trigger: "blur" },
           {
             min: 3,
             max: 20,
-            message: '长度在 3 到 20 个字符',
-            trigger: 'blur',
+            message: "长度在 3 到 20 个字符",
+            trigger: "blur",
           },
         ],
         code: [
-          { required: true, message: '请输入活动名称', trigger: 'blur' },
+          { required: true, message: "请输入活动名称", trigger: "blur" },
           {
             min: 3,
             max: 20,
-            message: '长度在 3 到 20 个字符',
-            trigger: 'blur',
+            message: "长度在 3 到 20 个字符",
+            trigger: "blur",
           },
         ],
       },
-    }
+    };
   },
   watch: {
-    'form.informType': {
+    "form.informType": {
       handler(newVal, oldVal) {
-        if (newVal == 'ROLE') {
-          this.$set(this.form, 'toRole', [])
-          this.$delete(this.form, 'toUser')
+        if (newVal == "ROLE") {
+          this.$set(this.form, "toRole", []);
+          this.$delete(this.form, "toUser");
         }
-        if (newVal == 'USER') {
-          this.$set(this.form, 'toUser', [])
-          this.$delete(this.form, 'toRole')
+        if (newVal == "USER") {
+          this.$set(this.form, "toUser", []);
+          this.$delete(this.form, "toRole");
         }
       },
       immediate: true,
@@ -148,121 +154,121 @@ export default {
   },
   computed: {
     ...mapState({
-    //   departmentList: (state) => state.department.departmentList,
+      //   departmentList: (state) => state.department.departmentList,
       listSelect: (state) => state.role.roleListSelect,
       tagListSelect: (state) => state.tag.tagListSelect,
     }),
     toUser() {
-      return this.form.toUser
+      return this.form.toUser;
     },
   },
-  created() {
-    this.initFilter()
+  activated() {
+    this.initFilter();
   },
   methods: {
     initFilter() {
       this.$store
-        .dispatch('role/getRoleListSelect')
+        .dispatch("role/getRoleListSelect")
         .then(() => {})
         .catch((err) => {
           this.$message({
-            type: 'error',
-            message: err || '初始化失败',
-          })
-        })
+            type: "error",
+            message: err || "初始化失败",
+          });
+        });
 
-    //   this.$store
-    //     .dispatch('department/getDepartmentListAll')
-    //     .then(() => {})
-    //     .catch((err) => {
-    //       this.$message({
-    //         type: 'error',
-    //         message: err || '初始化失败',
-    //       })
-    //     })
+      //   this.$store
+      //     .dispatch('department/getDepartmentListAll')
+      //     .then(() => {})
+      //     .catch((err) => {
+      //       this.$message({
+      //         type: 'error',
+      //         message: err || '初始化失败',
+      //       })
+      //     })
 
       this.$store
-        .dispatch('tag/getListSelect')
+        .dispatch("tag/getListSelect")
         .then(() => {})
         .catch((err) => {
           this.$message({
-            type: 'error',
-            message: err || '初始化失败',
-          })
-        })
+            type: "error",
+            message: err || "初始化失败",
+          });
+        });
     },
     handleConfirm() {
-      if (this.form.informType == 'USER') {
+      if (this.form.informType == "USER") {
         this.form.toUser = this.userSelects.map((item) => {
-          return item.uuid
-        })
+          return item.uuid;
+        });
       } else {
-        this.form.toRole = this.toRoles
+        this.form.toRole = this.toRoles;
       }
-      this.form.setTag = this.tagSelects
-      const payload = this.form
+      this.form.setTag = this.tagSelects;
+      const payload = this.form;
 
-      this.$refs['form'].validate((valid) => {
+      this.$refs["form"].validate((valid) => {
         if (valid) {
           if (this.form.words.length) {
             this.$store
-              .dispatch('sensitive/add', payload)
+              .dispatch("sensitive/add", payload)
               .then(() => {
                 this.$message({
-                  type: 'success',
-                  message: '操作成功',
-                })
-                this.handleCancel()
-                this.handleRefresh()
+                  type: "success",
+                  message: "操作成功",
+                });
+                this.handleCancel();
+                this.handleRefresh();
               })
               .catch((err) => {
                 this.$message({
-                  type: 'error',
-                  message: '操作失败',
-                })
-              })
+                  type: "error",
+                  message: "操作失败",
+                });
+              });
           } else {
             this.$message({
-              type: 'warning',
-              message: '请输入敏感词',
-            })
+              type: "warning",
+              message: "请输入敏感词",
+            });
           }
         } else {
           this.$message({
-            type: 'error',
-            message: '请检查输入',
-          })
+            type: "error",
+            message: "请检查输入",
+          });
         }
-      })
+      });
     },
     handleCancel() {
-      this.$parent.$parent.dialogVisible = false
+      this.$parent.$parent.dialogVisible = false;
     },
     handleClose(tag) {
-      this.form.words.splice(this.form.words.indexOf(tag), 1)
+      this.form.words.splice(this.form.words.indexOf(tag), 1);
     },
 
     showInput() {
-      this.inputVisible = true
+      this.inputVisible = true;
       this.$nextTick((_) => {
-        this.$refs.saveTagInput.$refs.input.focus()
-      })
+        this.$refs.saveTagInput.$refs.input.focus();
+      });
     },
 
     handleInputConfirm() {
-      let inputValue = this.inputValue
+      let inputValue = this.inputValue;
       if (inputValue) {
-        this.form.words.push(inputValue)
+        this.form.words.push(inputValue);
       }
-      this.inputVisible = false
-      this.inputValue = ''
+      this.inputVisible = false;
+      this.inputValue = "";
     },
 
     handleRefresh() {
-      this.$bus.$emit('handleRefresh')
+      this.$bus.$emit("handleRefresh");
     },
   },
-}
+};
 </script>
 
 <style lang="scss">
